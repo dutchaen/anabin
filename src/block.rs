85ea7655286f5e9bbbbb75686b74d6a6c63b53ff
dam <127336265+dutchaen@users.noreply.github.com>
2023-04-23 19:10:11 -0700

    Create block.rs

---
+++ src/block.rs
@@ -0,0 +1,12 @@
+pub trait Block {
+    fn wait(self) -> <Self as futures::Future>::Output
+        where Self: Sized, Self: futures::Future
+    {
+        futures::executor::block_on(self)
+    }
+}
+
+impl<F,T> Block for F
+    where F: futures::Future<Output = T>
+{}
+
